5df957ac7bfe782b585b78b0699753b52def0d54
Marcel Joshua <marceljoshua69@gmail.com>
2024-07-13 00:38:08 +0700

    initial artifact push

---
+++ src/main/java/org/synrgy/setara/SetaraApiServiceApplication.java
@@ -0,0 +1,13 @@
+package org.synrgy.setara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetaraApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetaraApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/synrgy/setara/SetaraApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.synrgy.setara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetaraApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
