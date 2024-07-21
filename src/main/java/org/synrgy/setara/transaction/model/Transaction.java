4ef2f708255f481494fc2be5c4b7565358d86026
Marcel Joshua <marceljoshua69@gmail.com>
2024-07-22 00:02:00 +0700

    [chore] replace wildcard lombok imports with explicit imports

--- src/main/java/org/synrgy/setara/transaction/model/Transaction.java
+++ src/main/java/org/synrgy/setara/transaction/model/Transaction.java
@@ -1,7 +1,19 @@
 package org.synrgy.setara.transaction.model;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.synrgy.setara.common.model.Auditable;
 import org.synrgy.setara.user.model.User;
 import org.synrgy.setara.vendor.model.Bank;
