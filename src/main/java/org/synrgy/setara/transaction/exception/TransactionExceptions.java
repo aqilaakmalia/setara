6fcc938567a306e0126632d4000907689321d586
Daffa Sulthon Kautsar <sulthondaffakautsar@gmail.com>
2024-07-24 12:54:08 +0700

    feat(api): Added bca-transfer api for transfers between bca banks

--- src/main/java/org/synrgy/setara/transaction/exception/TransactionExceptions.java
+++ src/main/java/org/synrgy/setara/transaction/exception/TransactionExceptions.java
@@ -20,6 +20,12 @@ public class TransactionExceptions {
         }
     }
 
+    public static class DestinationAccountNotFoundException extends RuntimeException {
+        public DestinationAccountNotFoundException(String message) {
+            super(message);
+        }
+    }
+
     public static class UserNotFoundException extends RuntimeException {
         public UserNotFoundException(String message) {
             super(message);
