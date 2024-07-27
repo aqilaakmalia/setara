7b88a1f3f7be075756a5eb6173961f1204fe1c40
aqilahakmalia <aqilaakmalia98@gmail.com>
2024-07-27 15:14:57 +0700

    delete unused code

--- src/main/java/org/synrgy/setara/vendor/repository/EwalletRepository.java
+++ src/main/java/org/synrgy/setara/vendor/repository/EwalletRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.synrgy.setara.vendor.model.Ewallet;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
